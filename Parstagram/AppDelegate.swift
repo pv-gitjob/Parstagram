f63409588599f3fb8822a11dd87ba9890ff03975
Praveen Vandeyar <praveenvandeyar@gmail.com>
2020-02-10 16:16:44 -0800

    Complete Part 1

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Parstagram"
+                configuration.server = "https://infinite-escarpment-95825.herokuapp.com/parse"
+            })
+        )
+        
         return true
     }
 
---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Praveen V on 2/10/20.
+//  Copyright © 2020 Praveen Vandeyar. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
